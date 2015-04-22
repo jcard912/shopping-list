8418480e538b321a097e5a13d7cc2200bc11befa
jcard912 <jcard912@gmail.com>
2015-04-22 12:35:58 -0400

    commit with jQuery

---
+++ js/list.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+	var itemList = 0;
+	var check = '<span class="check"><i class="fa fa-check"></i></span>'
+	var circle = '<span class="circle"><i class="fa fa-check-circle"></i></span>'
+	var x = '<span class="x"><i class="fa fa-times"></i></span>'
+	
+	//this function is to allow the user to press the enter key when submitting text
+	
+	function getItem() {
+		$('.enter-text').keydown(function (enter) {
+			if (enter.keyCode === 13) {
+				addItem();
+			}
+		});
+	}
+	
+	getItem();
+	
+	//this takes the value and adds it to the list
+
+	function addItem() {
+		var item =$('.enter-text').val();
+		var newItem = '<p class="input">' + check + circle + item + x + '</p>';
+		$('.user-list').prepend(newItem).fadeIn(500);
+		$('.enter-text').val('');
+	}
+		
+});
+	
+	
+	
+	
+	
+	//reset button
+	$(document).on('click', '.reset', function() {
+		$('.user-list').empty()
+		itemList = 0;
+	});
+	
+	
+	//check off items
+	$(document).on('click', '.check', function() {
+		$(this).closest('p').removeClass('input').addClass('complete');
+		$(this).hide();
+		$('.circle').show();
+		
+	});
+	
+	//uncheck
+	$(document).on('click', '.circle' , function() {
+		$(this).closest('p').removeClass('complete').addClass('input');
+		$(this).hide();
+		$('.check').show();
+	});
+	
+	
+	
+	
+	
+	// delete
+	$(document).on('click', '.x', function() {
+		$(this).closest('p').fadeOut(500);
+	});
+	
+	
